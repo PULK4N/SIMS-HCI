5fb9b58af17e857416d518690550c1ff546688ef
Mateja0312 <mateja.miletic5@gmail.com>
2021-06-08 13:00:56 +0200

    Design pattern - Adapter (working with multiple libraries for document generation)

---
+++ Project code/HospitalApp/HospitalApp/Adapter/SyncfusionWordAdapter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Syncfusion.DocIO.DLS;
+using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace HospitalApp.Adapter
+{
+    class SyncfusionWordAdapter : DocumentGeneratorInterface
+    {
+        public void CreateDocument(string text)
+        {
+            using (WordDocument document = new WordDocument())
+            {
+                //Adds a section and a paragraph to the document
+                document.EnsureMinimal();
+                //Appends text to the last paragraph of the document
+                document.LastParagraph.AppendText(text);
+                //Saves the Word document
+                document.Save("Output.docx");
+            }
+        }
+    }
+}
